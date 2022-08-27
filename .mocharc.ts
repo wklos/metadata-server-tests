35894014ef01d14beb15b46c9761cb04685797a9
wklos <wojtekklos@gmail.com>
2022-08-27 16:39:25 +0200

    Initial commit

---
+++ .mocharc.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  reporter: 'mochawesome',
+  'reporter-options': [
+    'overwrite=true',
+    'reportTitle=Metadata server tests',
+    'code=false',
+    'charts=true',
+    'inline=true',
+    'reportDir=reports',
+  ],
+};
